0192f3cf925abdb6a136fb154724ea1aeca29103
johnnywwy <johnnywwy@gmail.com>
2022-03-06 23:35:46 +0800

    展示和创建标签

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,13 +1,17 @@
 const localStorageKayName = 'recordList';
 
 const recordListModel = {
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   clone(data: RecordItem[]| RecordItem) {
     return JSON.parse(JSON.stringify(data));
   },
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   fetch() {
     return JSON.parse(window.localStorage.getItem(localStorageKayName) || '[]') as RecordItem[];
 
   },
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   save(data: RecordItem[]) {
     return window.localStorage.setItem(localStorageKayName, JSON.stringify(data));
   }
