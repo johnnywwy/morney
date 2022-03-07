f30e54389aed335c1242a88071b9b60d183e2cdf
johnnywwy <johnnywwy@gmail.com>
2022-03-07 22:33:31 +0800

    解决 id 生成问题,并保存到本地

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -4,7 +4,7 @@
       <span class="name">{{ this.fieldName }}</span>
       <input type="text" :value="value"
              @input="onValueChanged($event.target.value)"
-             :placeholder="this.placeholder">
+             :placeholder="placeholder">
     </label>
   </div>
 </template>
@@ -21,7 +21,6 @@ export default class FormItem extends Vue {
   @Prop({required: true}) fieldName!: string;
   @Prop() placeholder?: string;
 
-  @Watch('value')
   onValueChanged(value: string) {
     this.$emit('update:value', value);
   }
