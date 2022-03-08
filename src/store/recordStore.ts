2286056968f9cf1451b73f8c7b6de8fb924a1513
johnnywwy <johnnywwy@gmail.com>
2022-03-08 23:36:19 +0800

    完成 tagRecord 重构

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -2,21 +2,19 @@ import clone from '@/lib/clone';
 
 const localStorageKayName = 'recordList';
 
-let data: RecordItem[] | undefined = undefined;
-
 const recordStore = {
-  recordList: data,
+  recordList: [] as RecordItem[],
   fetchRecords() {
-    data = JSON.parse(window.localStorage.getItem(localStorageKayName) || '[]') as RecordItem[];
-    return data;
+    this.recordList = JSON.parse(window.localStorage.getItem(localStorageKayName) || '[]') as RecordItem[];
+    return this.recordList;
   },
   saveRecord() {
-    window.localStorage.setItem(localStorageKayName, JSON.stringify(data));
+    window.localStorage.setItem(localStorageKayName, JSON.stringify(this.recordList));
   },
-  createRecord: (record: RecordItem) => {
+  createRecord(record: RecordItem) {
     const record2: RecordItem = clone(record);
     record2.createAt = new Date();
-    data && data.push(record2);
+    this.recordList && this.recordList.push(record2);
     recordStore.saveRecord();
 
   }
--- src/store/tagStore.ts
+++ src/store/tagStore.ts
@@ -1,5 +1,70 @@
-import tagListModel from '@/models/tagListModel';
+import createId from '@/lib/createId';
 
-export default {
-  tagList: tagListModel.fetch()
+const localStorageKayName = 'tagList';
+
+const tagStore = {
+  tagList: [] as Tag[],
+
+  fetchTags() {
+    this.tagList = JSON.parse(window.localStorage.getItem(localStorageKayName) || '[]');
+    return this.tagList;
+  },
+
+  findTag(id: string) {
+    return this.tagList.filter(t => t.id === id)[0];
+  },
+
+  updateTag(id: string, name: string) {
+    const idList = this.tagList.map(item => item.id);
+    if (idList.indexOf(id) >= 0) {
+      const names = this.tagList.map(item => item.name);
+      if (names.indexOf(name) >= 0) {
+        return 'duplicated';
+      } else {
+        const tag = this.tagList.filter(item => item.id === id)[0];
+        tag.name = name;
+        this.saveTags();
+        return 'success';
+      }
+    } else {
+      return 'not found';
+    }
+  },
+
+  createTag(name: string) {
+    const names = this.tagList.map(item => item.name);
+    if (names.indexOf(name) >= 0) {
+      window.alert('标签重复了');
+      return 'duplicated';
+    }
+    const id = createId().toString();
+    this.tagList.push({id: id, name: name});
+    this.saveTags();
+    window.alert('添加成功');
+    return 'success';
+  },
+
+  saveTags() {
+    window.localStorage.setItem(localStorageKayName, JSON.stringify(this.tagList));
+  },
+
+
+  removeTag(id: string) {
+    let index = -1;
+    for (let i = 0; i < this.tagList.length; i++) {
+      if (this.tagList[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+    console.log('index');
+    console.log(index);
+    this.tagList.splice(index, 1);
+    this.saveTags();
+    return true;
+  }
 };
+
+tagStore.fetchTags();
+
+export default tagStore;
