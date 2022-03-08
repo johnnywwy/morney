4bc053dd929c559b744fd36640b5b949abc9ab98
johnnywwy <johnnywwy@gmail.com>
2022-03-08 16:01:56 +0800

    封装 window.createTag

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -5,3 +5,24 @@ type RecordItem = {
   amount: number //数据类型
   createAt?: Date | undefined//类
 }
+
+interface Window {
+  tagList: Tag[];
+  createTag: (name: string) => void;
+}
+
+type Tag = {
+  id: string
+  name: string
+
+}
+
+type TagListModel = {
+  data: Tag[];
+  fetch: () => Tag[];
+  create: (name: string) => 'success' | 'duplicated';// success表示成功，duplicated表示重复
+  update: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
+  save: () => void
+  remove: (id: string) => boolean
+}
+
--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,21 +1,8 @@
 import createId from '@/lib/createId';
 
-const localStorageKayName = 'recordList';
+const localStorageKayName = 'tagList';
 
-type Tag = {
-  id: string
-  name: string
 
-}
-
-type TagListModel = {
-  data: Tag[];
-  fetch: () => Tag[];
-  create: (name: string) => 'success' | 'duplicated';// success表示成功，duplicated表示重复
-  update: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
-  save: () => void
-  remove: (id: string) => boolean
-}
 const tagListModel: TagListModel = {
   data: [],
   fetch() {
