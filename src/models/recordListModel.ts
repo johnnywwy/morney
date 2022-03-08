061556097136fc723221e3b354794e6de7d8c63c
johnnywwy <johnnywwy@gmail.com>
2022-03-08 12:41:58 +0800

    重新封装 recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,19 +1,25 @@
 const localStorageKayName = 'recordList';
 
 const recordListModel = {
-
+  data: [] as RecordItem[],
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  clone(data: RecordItem[]| RecordItem) {
+  clone(data: RecordItem[] | RecordItem) {
     return JSON.parse(JSON.stringify(data));
+  },
+
+  create() {
+
   },
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   fetch() {
-    return JSON.parse(window.localStorage.getItem(localStorageKayName) || '[]') as RecordItem[];
+    this.data = JSON.parse(window.localStorage.getItem(localStorageKayName) || '[]') as RecordItem[];
+    return this.data;
 
   },
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  save(data: RecordItem[]) {
-    return window.localStorage.setItem(localStorageKayName, JSON.stringify(data));
+  save() {
+    window.localStorage.setItem(localStorageKayName, JSON.stringify(this.data));
+
   }
 };
 export default recordListModel;
