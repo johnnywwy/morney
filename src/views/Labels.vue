af22edddd1824060797ce81da6412d1f1b9b3a08
johnnywwy <johnnywwy@gmail.com>
2022-03-09 16:22:15 +0800

    完成 tagRecord 重构

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -17,7 +17,6 @@
 
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
-//import tagListModel from '@/models/tagListModel';
 import Button from '@/components/Button.vue';
 import store from '@/store/index2';
 
