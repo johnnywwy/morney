541a64224a7f26b7818669891f9e9537ed1db8b2
johnnywwy <johnnywwy@gmail.com>
2022-03-10 15:35:20 +0800

    成功全部使用 Vuex 来进行数据管理

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,7 +6,7 @@
       <Icon class="rightIcon"/>
     </div>
     <div class="form-wrapper">
-      <FormItem :value="tag.name"
+      <FormItem :value="currentTag.name"
                 @update:value="update"
                 field-name="标签名"
                 placeholder="请输入标签名"/>
@@ -31,7 +31,7 @@ import Button from '@/components/Button.vue';
 })
 export default class EditLabel extends Vue {
 
-  get tag() {
+  get currentTag() {
     return this.$store.state.currentTag;
   }
 
@@ -40,21 +40,21 @@ export default class EditLabel extends Vue {
     const id = this.$route.params.id;
     this.$store.commit('fetchTags');
     this.$store.commit('setCurrentTag', id);
-    if (!this.tag) {
+    if (!this.currentTag) {
       this.$router.replace('/404');
     }
   }
 
   update(name: string) {
-    if (this.tag) {
+    if (this.currentTag) {
       this.$store.commit('updateTag',
-          {id: this.tag.id, name});
+          {id: this.currentTag.id, name});
     }
   }
 
   remove() {
-    if (this.tag) {
-      this.$store.commit('removeTag', this.tag.id);
+    if (this.currentTag) {
+      this.$store.commit('removeTag', this.currentTag.id);
     }
   }
 
